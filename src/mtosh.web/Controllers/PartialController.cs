3aeab414299d304d3f3b2490738a93ff11b584f8
dmackintosh <david@mtosh.net>
2017-04-03 12:15:31 +1000

    initial 2

---
+++ src/mtosh.web/Controllers/PartialController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mtosh.web.Controllers
+{
+    public class PartialController : Controller
+    {
+        public IActionResult AboutComponent() => PartialView();
+
+        public IActionResult AppComponent() => PartialView();
+
+        public IActionResult ContactComponent() => PartialView();
+
+        public IActionResult IndexComponent() => PartialView();
+    }
+}
